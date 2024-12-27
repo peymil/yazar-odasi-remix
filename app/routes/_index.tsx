3ca7861f92b888843f77e3c0b9b98cc6058ce47c
Emre Badem <emrebadem_@hotmail.com>
2024-12-27 10:01:07 +0300

    /

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,8 @@ import {useOptionalUser} from "~/lib/authUtils";
 
 export const meta: MetaFunction = () => {
     return [
-        {title: "New Remix App"},
-        {name: "description", content: "Welcome to Remix!"},
+        {title: "Yazar Odasi"},
+        {name: "description", content: "Yazarodasi is a place for writers."},
     ];
 };
 
