23adf6cbb4459e7367e13fec090c842c548cd44b
peymil <emrebadem_@hotmail.com>
2026-02-19 14:12:55 +0300

    fix: update SQL queries to include wildcard search for title and description

--- prisma/sql/projectSearch.sql
+++ prisma/sql/projectSearch.sql
@@ -6,20 +6,21 @@ SELECT
     p.*,
     up.user_id,
     array_agg(DISTINCT jsonb_build_object(
-         pg.id,
-         pg.genre_name
-    )) as genres,
+        'id', pg.id,
+        'genre_name', pg.genre_name
+    )) FILTER (WHERE pg.id IS NOT NULL) as genres,
     array_agg(DISTINCT jsonb_build_object(
-         pt.id,
-         pt.tag_name
-    )) as tags
+        'id', pt.id,
+        'tag_name', pt.tag_name
+    )) FILTER (WHERE pt.id IS NOT NULL) as tags,
+    COUNT(*) OVER() as total_count
 FROM user_profile_project p
 JOIN user_profile up ON p.profile_id = up.id
 LEFT JOIN project_projectgenre ppg ON p.id = ppg.project_id
 LEFT JOIN project_genre pg ON ppg.project_genre_id = pg.id
 LEFT JOIN project_projecttag ppt ON p.id = ppt.project_id
 LEFT JOIN project_tag pt ON ppt.project_tag_id = pt.id
-WHERE p.plot_title ILIKE $1
+WHERE p.plot_title ILIKE '%' || $1 || '%'
 GROUP BY p.id, up.user_id
 ORDER BY p.plot_title
 LIMIT $2 OFFSET $3
